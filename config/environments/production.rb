d22a89925624b7cf49894e80da67668f289efa92
Arkadii Chugunov <arkadii@Arkadiis-MacBook-Pro-2.local>
2025-05-23 19:59:10 +0200

    Remove solid_queue adapter from production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,16 +42,13 @@ Rails.application.configure do
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
-
-  # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "https://testapp1-fg3d.onrender.com/" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
